99dd0921912abee456a08e5714a4e0d31e71f235
Andrew Magana <amagana3@latlmacw29vjhd4.int.dealer.com>
2021-04-18 23:56:43 -0400

    logging.

--- feedback_checker.py
+++ feedback_checker.py
@@ -2,6 +2,8 @@
 # feedback_checker.py
 import os
 
+import logging
+
 import discord
 from dotenv import load_dotenv
 
@@ -14,6 +16,7 @@ from dotenv import load_dotenv
 #   if they haven't, then we decline the feedback and tell them to give feedback to previous user.
 #       Send them the message in context to give feedback.
 
+logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(funcName)s:%(lineno)d - %(message)s')
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -25,8 +28,7 @@ client = discord.Client()
 @client.event
 async def on_ready():
     guild = discord.utils.get(client.guilds, name=GUILD)
-    print(f'{client.user} is connected to the following GUILD: {guild.name} (ID: {guild.id}')
-    print()
+    logging.info(f'{client.user} is connected to the following GUILD: {guild.name} (ID: {guild.id}')
 
 
 @client.event
@@ -37,24 +39,24 @@ async def on_message(message):
 
     # Should be in feedback channel
     if message.channel.name == 'feedback':
-
-        temp_logg_dict = {
+        general_log = {
             "author": message.author.name,
             "content": message.content,
             "attachments": message.attachments
         }
 
-        print(temp_logg_dict)
-        print()
+        logging.info(general_log)
 
         # Verify links
         if ('soundcloud.com' or 'soundcloud.app.goo.gl' or 'dropbox.com') in message.content:
+            logging.info("link found: ", message.content)
             if not await validate_feedback(message):
                 await message.delete()
                 await deny_feedback(message)
 
         # TODO: Test this functionality
         if len(message.attachments) > 1 and message.attachments[0].url.contains('.mp3' or '.mp4a' or '.wav' or '.flac'):
+            logging.info("attachment found: ", message.content)
             if not await validate_feedback(message):
                 await message.delete()
                 await deny_feedback(message)
@@ -85,15 +87,19 @@ async def on_message(message):
 
 # This exists to give the last feedback overall (even if you're the final submission)
 async def last_feedback(message) -> tuple:
+    logging.info("last_feedback() called.")
     count = 0
     messages = await message.channel.history(limit=100).flatten()
     for m in messages:
+        logging.info("message history: " + m)
         if ("soundcloud.com" or 'soundcloud.app.goo.gl' or 'dropbox.com') in m.content and count < 1:
+            logging.info("found previous feedback " + m)
             # Look for previous feedback
             count += 1
             return m.author.name, m.content, m.jump_url, m.author.id
 
         if len(m.attachments) > 1 > count and m.attachments[0].url.contains('.mp3' or '.mp4a' or '.wav' or '.flac'):
+            logging.info("found previous feedback " + m)
             # Look for previous feedback
             count += 1
             return m.author.name, m.content, m.jump_url, m.author.id
@@ -101,18 +107,22 @@ async def last_feedback(message) -> tuple:
 
 # This is to get the previous feedback that DOES NOT INCLUDE the denied one (since we gotta submit to trigger this)
 async def previous_feedback(message) -> tuple:
+    logging.info("previous_feedback() called.")
     count = 0
     messages = await message.channel.history(limit=100).flatten()
     for m in messages:
+        logging.info("message history: " + m)
         # This stops from showing the person who just submitted.
         if message.author.id == m.author.id:
             continue
         if ("soundcloud.com" or 'soundcloud.app.goo.gl' or 'dropbox.com') in m.content and count < 1:
+            logging.info("found previous feedback " + m)
             # Look for previous feedback
             count += 1
             return m.author.name, m.content, m.jump_url, m.author.id, m
 
         if len(m.attachments) > 1 > count and m.attachments[0].url.contains('.mp3' or '.mp4a' or '.wav' or '.flac'):
+            logging.info("found previous feedback " + m)
             # Look for previous feedback
             count += 1
             return m.author.name, m.content, m.jump_url, m.author.id, m
@@ -127,21 +137,21 @@ async def previous_feedback(message) -> tuple:
 # This method checks if the user who submitted feedback has given feedback to previous poster
 # Returns a boolean.
 async def validate_feedback(message):
+    logging.info("validate_feedback() called.")
     # Okay let's get the username of requester and the last feedback submitter.
     curr_feedback_user = message.author.name
     curr_feedback_user_id = message.author.id
 
-    print("New person submitted feedback: " + curr_feedback_user)
-    print()
+    logging.info("new feedback submitted by: " + curr_feedback_user)
 
     resp = await previous_feedback(message)
+    logging.info("found previous feedback ", resp)
 
     prev_feedback_user = resp[0]
     prev_feedback_user_id = resp[3]
     previous_feedback_message = resp[4]
-    print("Previous Feedback ID: ", previous_feedback_message.id)
-    print("Previous Feedback author: " + prev_feedback_user)
-    print()
+    logging.info("previous feedback ID: ", previous_feedback_message.id)
+    logging.info("previous feedback author: " + prev_feedback_user)
 
     # Check history, has the requester replied to the submitter?
     messages = await message.channel.history(limit=100).flatten()
@@ -160,8 +170,8 @@ async def validate_feedback(message):
                     for mention in y.mentions:
                         if mention.id == prev_feedback_user_id:
                             # We know the new feedback submitter has replied to the previous feedback author.
-                            print("Feedback message:", y.content)
-                            print("Length of feedback:", len(y.content))
+                            logging.info("Feedback message:", y.content)
+                            logging.info("Length of feedback:", len(y.content))
                             if len(y.content) >= 100:
                                 return True
                             else:
@@ -173,7 +183,7 @@ async def validate_feedback(message):
 
 
 async def deny_feedback(message):
-    print("Feedback denied for:", message.author.name)
+    logging.info("Feedback denied for: ", message.author.name)
 
     last_feedback_info = await previous_feedback(message)
     embed_var = discord.Embed(title="Last Feedback Request", description="By: " + last_feedback_info[0],
